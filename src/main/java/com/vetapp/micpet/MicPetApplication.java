ec2ebe7994f54a6d39aa19a52efe794492807f7b
Victor Lu <victorlu.upv@gmail.com>
2024-06-14 19:36:15 +0200

    First commit

---
+++ src/main/java/com/vetapp/micpet/MicPetApplication.java
@@ -0,0 +1,13 @@
+package com.vetapp.micpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicPetApplication.class, args);
+	}
+
+}
